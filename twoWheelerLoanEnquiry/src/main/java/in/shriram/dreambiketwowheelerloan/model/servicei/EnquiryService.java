a93f1ed619ce0a6b31ec4983364548153a53c99a
payalkhawase <payal.khawase14@gmail.com>
2025-02-18 22:17:22 +0530

    add post api

--- twoWheelerLoanEnquiry/src/main/java/in/shriram/dreambiketwowheelerloan/model/servicei/EnquiryService.java
+++ twoWheelerLoanEnquiry/src/main/java/in/shriram/dreambiketwowheelerloan/model/servicei/EnquiryService.java
@@ -1,5 +1,9 @@
 package in.shriram.dreambiketwowheelerloan.model.servicei;
 
+import in.shriram.dreambiketwowheelerloan.model.model.Enquiry;
+
 public interface EnquiryService {
+
+	public Enquiry setAllData(Enquiry e);
 
 }
--- twoWheelerLoanEnquiry/src/main/java/in/shriram/dreambiketwowheelerloan/model/serviceimpl/EnquiryServiceImpl.java
+++ twoWheelerLoanEnquiry/src/main/java/in/shriram/dreambiketwowheelerloan/model/serviceimpl/EnquiryServiceImpl.java
@@ -3,6 +3,7 @@ package in.shriram.dreambiketwowheelerloan.model.serviceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import in.shriram.dreambiketwowheelerloan.model.model.Enquiry;
 import in.shriram.dreambiketwowheelerloan.model.repository.EnquiryRepo;
 import in.shriram.dreambiketwowheelerloan.model.servicei.EnquiryService;
 
@@ -11,4 +12,11 @@ public class EnquiryServiceImpl implements EnquiryService{
 
 	@Autowired
 	EnquiryRepo er;
+
+	@Override
+	public Enquiry setAllData(Enquiry e) {
+	
+		Enquiry en=er.save(e);
+		return en;
+	}
 }
