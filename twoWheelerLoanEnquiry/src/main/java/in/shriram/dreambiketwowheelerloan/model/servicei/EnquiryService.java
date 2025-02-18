b70ba4723fe187d8f8b98751d3f845b97d16a76b
GajananChavhan-pixel <shubhamchavhan077@gmail.com>
2025-02-18 22:50:27 +0530

    Added get request for getting single data

--- twoWheelerLoanEnquiry/src/main/java/in/shriram/dreambiketwowheelerloan/model/servicei/EnquiryService.java
+++ twoWheelerLoanEnquiry/src/main/java/in/shriram/dreambiketwowheelerloan/model/servicei/EnquiryService.java
@@ -1,9 +1,14 @@
 package in.shriram.dreambiketwowheelerloan.model.servicei;
 
+import java.util.List;
+
 import in.shriram.dreambiketwowheelerloan.model.model.Enquiry;
 
 public interface EnquiryService {
 
 	public Enquiry setAllData(Enquiry e);
 
+	public Enquiry getAllEnquiry();
+
+	
 }
