d012bf87abde0f2923b131e1b06db3604c1259ef
sujata-cell <srjp90@gmail.com>
2025-02-19 21:59:47 +0530

    added cibil validation by sujata

--- twoWheelerLoanEnquiry/src/main/java/in/shriram/dreambiketwowheelerloan/model/model/Cibil.java
+++ twoWheelerLoanEnquiry/src/main/java/in/shriram/dreambiketwowheelerloan/model/model/Cibil.java
@@ -4,6 +4,11 @@ import java.util.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
@@ -11,8 +16,15 @@ import lombok.Data;
 public class Cibil {
 	@Id
 	private int cibilId;
+	@Min(300)
+	@Max(900)
 	private int cibilScore;
+	@NotEmpty
 	private Date cibilscoredDateTime;
-	private String status;
+	@NotBlank
+	@NotNull
+	private enum status{Excellent,VeryGood,Good};
+	@NotBlank
+	@NotEmpty
 	private String cibilRemark;
 }
